c9c218d6e78095a9188f9d17157cd79ab82fb512
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-08-28 20:10:29 +0530

    Time: 0 ms (100%), Space: 11.1 MB (71.65%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -12,37 +12,52 @@ class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
         if(left==right) return head;
-            ListNode* first = NULL;
-            ListNode* second = NULL;
-            ListNode* third = NULL;
-            ListNode* fourth = NULL;
-            int count = 1;
-            ListNode* temp = head;
-            while(temp!=NULL){
-                if(count==left-1) first = temp;
-                if(count==left) second = temp;
-                if(count==right) third = temp;
-                if(count==right+1) fourth = temp;
-                temp = temp->next;
-                count++;
-            }
-            if(first!=NULL) first->next = NULL;
-            if(third!=NULL) third->next = NULL;
-        ListNode* prev = NULL;
-        ListNode* curr = second;
-        ListNode* t = second;
-        while(curr!=NULL){
-            t = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = t;
+        ListNode* first = NULL;
+        ListNode* second = NULL;
+        ListNode* third = NULL;
+        ListNode* fourth = NULL;
+        int count = 1;
+        ListNode* temp2 = head;
+        while(temp2!=NULL){
+            if(count==left-1) first = temp2;
+            if(count==left) second = temp2;
+            if(count==right) third = temp2;
+            if(count==right+1) fourth = temp2;
+            temp2 = temp2->next;
+            count++;
         }
         if(left==1){
-            head->next = fourth;
+            third->next = NULL;
+            ListNode* s = second;
+            ListNode* prev = NULL;
+            ListNode* curr = second;
+            ListNode* temp = second;
+            while(curr!=NULL){
+                temp = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = temp;
+            }
+            if(s!=NULL) s->next = fourth;
             return prev;
+
+        }
+
+        first->next = NULL;
+        third->next = NULL;
+        ListNode* save = second;
+        ListNode* prev = NULL;
+        ListNode* curr = second;
+        ListNode* temp = second;
+        while(curr!=NULL){
+            temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
         }
         first->next = prev;
-        second->next = fourth;
+        save->next = fourth;
+
         return head;
     }
 };
