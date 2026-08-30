cd96dca0918433cab1090969afcf56d1eddef6aa
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-08-30 19:22:16 +0530

    Time: 335 ms (5.1%), Space: 23.8 MB (17.5%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -17,17 +17,17 @@ public:
         return 1 + max(level(root->left),level(root->right));
     }
 
-    void traverse(TreeNode* root,int &max_ele){
+    void traverse(TreeNode* root,int &max_height){
         if(root==NULL) return;
-        int n = level(root->left) + level(root->right);
-        if(max_ele<n) max_ele = n;
-        traverse(root->left,max_ele);
-        traverse(root->right,max_ele);
+        int sum =level(root->left) + level(root->right);
+        if(max_height<sum) max_height = sum;
+        traverse(root->left,max_height);
+        traverse(root->right,max_height);
     }
 
     int diameterOfBinaryTree(TreeNode* root) {
-        int max_ele = 0;
-        traverse(root,max_ele);
-        return max_ele;
+        int max_height = INT_MIN;
+        traverse(root,max_height);
+        return max_height;
     }
 };
