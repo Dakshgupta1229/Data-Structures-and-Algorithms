8391a25def8aa7a47ebd45595220ab0af6dbaac1
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-08-30 17:40:02 +0530

    Time: 3 ms (91.24%), Space: 14.9 MB (96.57%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,21 +17,54 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+        Node* newHead = new Node(10);
+        Node* t = newHead;
         Node* temp = head;
-        map<Node*,Node*> m;
         while(temp!=NULL){
-            Node* t = new Node(temp->val);
-            m[temp] = t;
+            Node* tt = new Node(temp->val);
+            newHead->next = tt;
+            newHead = newHead->next;
             temp = temp->next;
         }
-        for(auto p:m){
-            Node* first = p.first;
-            Node* second = p.second;
-            Node* first_next = first->next;
-            second->next = m[first_next];
-            Node* first_random = first->random;
-            second->random = m[first_random];
+        t = t->next;
+        Node* newHead2 = new Node(10);
+        Node* t2 = newHead2;
+        while(head!=NULL && t!=NULL){
+            newHead2->next = head;
+            newHead2 = newHead2->next;
+            head = head->next;
+            newHead2->next = t;
+            newHead2 = newHead2->next;
+            t = t->next;
         }
-        return m[head];
+        t2 = t2->next;
+        Node* t3 = t2;
+        while(t3!=NULL){
+            Node* original_random = t3->random;
+            if(original_random==NULL){
+                t3->next->random = NULL;
+            }
+            else{
+                t3->next->random = original_random->next;
+            }
+            t3 = t3->next->next;
+        }
+        Node* result1 = new Node(10);
+        Node* r1 = result1;
+        Node* result2 = new Node(10);
+        Node* r2 = result2;
+        while(t2!=NULL){
+            result1->next = t2;
+            result1 = result1->next;
+            t2 = t2->next;
+            result2->next = t2;
+            result2 = result2->next;
+            t2 = t2->next;
+        }
+        if(result1!=NULL) result1->next = NULL;
+        if(result2!=NULL) result2->next = NULL;
+        head = r1->next;
+
+        return r2->next;
     }
 };
