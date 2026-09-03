2a186e54290e139a00813ebe9b4c83a0730439dc
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-03 12:43:37 +0530

    Time: 0 ms (100%), Space: 18.4 MB (73.36%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -12,28 +12,22 @@
 class Solution {
 public:
 
-    bool check(TreeNode* root1,TreeNode* root2,bool &flag){
+    bool check(TreeNode* root1,TreeNode* root2){
         if(root1==NULL && root2==NULL) return true;
-        if(root1==NULL || root2==NULL){
-            flag = false;
-            return false;
-        }
-        if(root1->val!=root2->val){
-            flag = false;
-            return false;
-        }
-        bool result1 = check(root1->left,root2->right,flag);
-        if(result1==false){
-            flag = false;
-            return false;
-        }
-        bool result2 = check(root1->right,root2->left,flag);
-        return result2;
+        if(root1!=NULL && root2==NULL) return false;
+        if(root1==NULL && root2!=NULL) return false;
+        if(root1->val!=root2->val) return false;
+        bool flag1 = check(root1->left,root2->right);
+        if(flag1==false) return flag1;
+        bool flag2 = check(root1->right,root2->left);
+        return flag2;
     }
 
     bool isSymmetric(TreeNode* root) {
-        bool flag = true;
-        check(root->left,root->right,flag);
-        return flag;
+        if(root==NULL) return true;
+        if(root->left==NULL && root->right==NULL) return true;
+        bool result = check(root->left,root->right);
+        return result;
+        
     }
 };
