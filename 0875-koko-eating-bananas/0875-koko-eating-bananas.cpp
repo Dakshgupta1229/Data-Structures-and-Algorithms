e41254f227820a80846c50500787d9e25027e619
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-05 18:57:52 +0530

    Time: 7 ms (69.43%), Space: 22.9 MB (58.68%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
 
-    bool check(vector<int> &piles,long long mid,int h){
+    long long check(vector<int> &piles,long long h,long long mid){
         long long count = 0;
         for(int i=0;i<piles.size();i++){
-            if(piles[i]<=mid) count++;
-            else{
-                count = count + (piles[i]/mid);
-                if(piles[i]%mid!=0) count++;
-            }
+            if((long long)piles[i]<=mid) count++;
+            else if((long long)piles[i]%mid==0) count = count + ((long long)piles[i]/mid);
+            else count = count + ((long long)piles[i]/mid) + 1;
         }
-        if(count<=h) return true;
-        return false;
+        return count;
     }
 
     int minEatingSpeed(vector<int>& piles, int h) {
-        int max_ele = INT_MIN;
+        long long low = 1;
+        long long max_ele = LLONG_MIN;
         for(int i=0;i<piles.size();i++){
             if(max_ele<piles[i]) max_ele = piles[i];
         }
-        int low = 1;
-        int high = max_ele;
-        int ans = -1;
+        long long high = max_ele;
+        long long ans = -1;
         while(low<=high){
             long long mid = low + (high-low)/2;
-            if(check(piles,mid,h)){
+            long long count = check(piles,h,mid);
+            if(count<=(long long)h){
                 ans = mid;
                 high = mid - 1;
             }
             else low = mid + 1;
         }
-        return ans;
+        return (int)ans;
     }
 };
