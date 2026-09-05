27c627226671258502b9b78065f3481c62b95c8e
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-05 18:20:18 +0530

    Time: 12 ms (50.1%), Space: 35.2 MB (19.66%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
 
-    bool check(vector<int> &weights,int w,int days){
-        int count = 0;
+    int check(vector<int> &weights,int days,int mid){
         int sum = 0;
+        int count = 0;
         for(int i=0;i<weights.size();i++){
-            if(sum+weights[i]<=w){
+            if(sum+weights[i]<=mid){
                 sum = sum + weights[i];
             }
             else{
+                sum = weights[i];
                 count++;
-                sum = 0;
-                sum += weights[i];
             }
         }
         if(sum!=0) count++;
-        if(count<=days) return true;
-        return false;
+        return count;
     }
 
     int shipWithinDays(vector<int>& weights, int days) {
-        int ans = -1;
         int max_ele = INT_MIN;
         int sum = 0;
         for(int i=0;i<weights.size();i++){
             if(max_ele<weights[i]) max_ele = weights[i];
             sum = sum + weights[i];
         }
-        int low = max_ele;
-        int high = sum;
+        long long low = max_ele;
+        long long high = sum;
+        int ans = -1;
         while(low<=high){
-            int mid = low + (high-low)/2;
-            if(check(weights,mid,days)){
+            long long mid = low + (high-low)/2;
+            int count = check(weights,days,(int)mid);
+            if(count<=days){
                 ans = mid;
                 high = mid - 1;
             }
             else low = mid + 1;
         }
         return ans;
-
     }
 };
