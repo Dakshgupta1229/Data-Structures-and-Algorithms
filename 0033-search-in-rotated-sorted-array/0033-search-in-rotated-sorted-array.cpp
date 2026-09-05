3b5922a19120071db833701750e2ccd8e4f79b90
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-05 17:44:47 +0530

    Time: 0 ms (100%), Space: 15.3 MB (36.97%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -3,20 +3,19 @@ public:
     int search(vector<int>& nums, int target) {
         if(nums.size()==1){
             if(nums[0]==target) return 0;
-            else return -1;
         }
         if(nums.size()==2){
             if(nums[0]==target) return 0;
             else if(nums[1]==target) return 1;
-            else return -1;
+            return -1;
         }
         int pivot_idx = -1;
         int low = 0;
         int high = nums.size()-1;
         while(low<=high){
             int mid = low + (high-low)/2;
-            if(mid==0) low = mid + 1;
-            else if(mid==nums.size()-1) high = mid - 1;
+            if(mid==0) low++;
+            else if(mid==nums.size()-1) high--;
             else if(nums[mid]<nums[mid-1] && nums[mid]<nums[mid+1]){
                 pivot_idx = mid;
                 break;
@@ -28,7 +27,6 @@ public:
             else if(nums[mid]>nums[0]) low = mid + 1;
             else high = mid - 1;
         }
-        cout<<pivot_idx;
         if(pivot_idx==-1){
             low = 0;
             high = nums.size()-1;
