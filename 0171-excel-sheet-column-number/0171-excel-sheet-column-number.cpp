d8f4957898bce9aaf65fbfe6b00060590f2558e8
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-06 17:32:51 +0530

    Time: 0 ms (100%), Space: 9 MB (7.62%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int sum = 0;
+        int idx = 0;
+        for(int i=columnTitle.size()-1;i>=0;i--){
+            sum = sum + (columnTitle[i]-65+1) * pow(26,idx);
+            idx++;
+        }
+        return sum;
+    }
+};
