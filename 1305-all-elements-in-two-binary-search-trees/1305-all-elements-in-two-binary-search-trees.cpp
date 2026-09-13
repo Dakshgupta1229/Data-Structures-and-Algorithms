d0e75f5e9b212c3870f1092e5b973b5e5401e29e
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-13 18:48:26 +0530

    Time: 8 ms (44.57%), Space: 86.3 MB (82.44%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -12,39 +12,18 @@
 class Solution {
 public:
 
-    void traverse(TreeNode* root,vector<int> &v1){
+    void traverse(TreeNode* root,vector<int> &v){
         if(root==NULL) return;
-        traverse(root->left,v1);
-        v1.push_back(root->val);
-        traverse(root->right,v1);
+        traverse(root->left,v);
+        v.push_back(root->val);
+        traverse(root->right,v);
     }
 
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
         vector<int> v;
-        vector<int> v1;
-        vector<int> v2;
-        traverse(root1,v1);
-        traverse(root2,v2);
-        int i=0;
-        int j=0;
-        while(i<v1.size() && j<v2.size()){
-            if(v1[i]<v2[j]){
-                v.push_back(v1[i]);
-                i++;
-            }
-            else{
-                v.push_back(v2[j]);
-                j++;
-            }
-        }
-        while(i<v1.size()){
-            v.push_back(v1[i]);
-            i++;
-        }
-        while(j<v2.size()){
-            v.push_back(v2[j]);
-            j++;
-        }
+        traverse(root1,v);
+        traverse(root2,v);
+        sort(v.begin(),v.end());
         return v;
     }
 };
