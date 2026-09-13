e191f13b04feea569a8d9a96e14a0aee694685ec
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-13 22:05:20 +0530

    Time: 0 ms (100%), Space: 16 MB (55.61%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -16,7 +16,6 @@ public:
         queue<pair<TreeNode*,int>> q;
         q.push({root,0});
         map<int,map<int,vector<int>>> m;
-        m[0][0].push_back(root->val);
         int level = 0;
         while(q.size()>0){
             int n = q.size();
@@ -24,16 +23,10 @@ public:
                 auto it = q.front();
                 q.pop();
                 TreeNode* temp = it.first;
-                int cnt = it.second;
-                if(temp->left!=NULL){
-                    q.push({temp->left,cnt-1});
-                    m[cnt-1][level].push_back(temp->left->val);
-                    
-                }
-                if(temp->right!=NULL){
-                    q.push({temp->right,cnt+1});
-                    m[cnt+1][level].push_back(temp->right->val);
-                }
+                int vertical = it.second;
+                m[vertical][level].push_back(temp->val);
+                if(temp->left!=NULL) q.push({temp->left,vertical-1});
+                if(temp->right!=NULL) q.push({temp->right,vertical+1});
             }
             level++;
         }
@@ -41,9 +34,10 @@ public:
             map<int,vector<int>> m1 = p.second;
             vector<int> v2;
             for(auto p1:m1){
+                cout<<p1.first<<" -> Level"<<endl;
                 vector<int> v1 = p1.second;
                 sort(v1.begin(),v1.end());
-                for(int j=0;j<v1.size();j++) v2.push_back(v1[j]);
+                for(int i=0;i<v1.size();i++) v2.push_back(v1[i]);
             }
             v.push_back(v2);
         }
