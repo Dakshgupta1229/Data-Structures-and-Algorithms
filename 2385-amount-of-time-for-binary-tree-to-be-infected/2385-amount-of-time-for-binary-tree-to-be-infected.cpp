0255e00b6ce87d377136fbe5fac45a69b69cfe25
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-14 00:44:20 +0530

    Time: 371 ms (14.16%), Space: 192.8 MB (63.07%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -11,50 +11,46 @@
  */
 class Solution {
 public:
-    int amountOfTime(TreeNode* root, int start) {
-        map<TreeNode*,TreeNode*> m;
-        queue<TreeNode*> q;
-        q.push(root);
-        TreeNode* target = NULL;
-        while(q.size()>0){
-            TreeNode* temp = q.front();
-            q.pop();
-            if(temp->val==start) target = temp;
-            if(temp->left!=NULL){
-                m[temp->left] = temp;
-                q.push(temp->left);
-            }
-            if(temp->right!=NULL){
-                m[temp->right] = temp;
-                q.push(temp->right);
-            }
-        }
-        set<TreeNode*> s;
-        queue<pair<TreeNode*,int>> q1;
-        q1.push({target,0});
-        s.insert(target);
-        int rst = 0;
-        while(q1.size()>0){
-            auto it = q1.front();
-            q1.pop();
-            TreeNode* temp = it.first;
-            int cnt = it.second;
-            rst = cnt;
-            if(m[temp]!=NULL && s.find(m[temp])==s.end()){
-                q1.push({m[temp],cnt+1});
-                s.insert(m[temp]);
-            }
-            if(temp->left!=NULL && s.find(temp->left)==s.end()){
-                q1.push({temp->left,cnt+1});
-                s.insert(temp->left);
-            }
-            if(temp->right!=NULL && s.find(temp->right)==s.end()){
-                q1.push({temp->right,cnt+1});
-                s.insert(temp->right);
-            }
 
+    void traverse(TreeNode* root,int start,TreeNode* &temp,map<TreeNode*,TreeNode*> &m){
+        if(root==NULL) return;
+        if(root->left!=NULL) m[root->left] = root;
+        if(root->right!=NULL) m[root->right] = root;
+        if(root->val==start){
+            temp = root;
         }
-        
-        return rst;
+        traverse(root->left,start,temp,m);
+        traverse(root->right,start,temp,m);
+    }
+
+    int amountOfTime(TreeNode* root, int start) {
+        TreeNode* temp = NULL;
+        map<TreeNode*,TreeNode*> m;
+        traverse(root,start,temp,m);
+        queue<pair<TreeNode*,int>> q;
+        q.push({temp,0});
+        set<TreeNode*> s;
+        s.insert(temp);
+        int result = 0;
+        while(q.size()>0){
+            auto it = q.front();
+            q.pop();
+            TreeNode* tmp = it.first;
+            int value = it.second;
+            result = value;
+            if(tmp->left!=NULL && s.find(tmp->left)==s.end()){
+                q.push({tmp->left,value+1});
+                s.insert(tmp->left);
+            }
+            if(tmp->right!=NULL && s.find(tmp->right)==s.end()){
+                q.push({tmp->right,value+1});
+                s.insert(tmp->right);
+            }
+            if(m.find(tmp)!=m.end() && s.find(m[tmp])==s.end()){
+                q.push({m[tmp],value+1});
+                s.insert(m[tmp]);
+            }
+        }
+        return result;
     }
 };
