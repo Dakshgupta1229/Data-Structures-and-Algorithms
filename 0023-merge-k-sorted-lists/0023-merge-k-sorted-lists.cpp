776cc975554840489c25fbff5cb6542526631751
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-15 08:45:14 +0530

    Time: 103 ms (10.33%), Space: 18.5 MB (48.73%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -17,22 +17,21 @@ public:
             ListNode* temp2 = lists[lists.size()-2];
             lists.pop_back();
             lists.pop_back();
-            ListNode* temp = new ListNode(1000);
-            ListNode* t = temp;
+            ListNode* newHead = new ListNode(100);
+            ListNode* t = newHead;
             while(temp1!=NULL && temp2!=NULL){
-                if(temp1->val<=temp2->val){
-                    temp->next = temp1;
-                    temp = temp->next;
+                if(temp1->val<temp2->val){
+                    newHead->next = temp1;
                     temp1 = temp1->next;
                 }
                 else{
-                    temp->next = temp2;
-                    temp = temp->next;
+                    newHead->next = temp2;
                     temp2 = temp2->next;
                 }
+                newHead = newHead->next;
             }
-            if(temp1!=NULL) temp->next = temp1;
-            if(temp2!=NULL) temp->next = temp2;
+            if(temp1!=NULL) newHead->next = temp1;
+            if(temp2!=NULL) newHead->next = temp2;
             lists.push_back(t->next);
         }
         return lists[0];
