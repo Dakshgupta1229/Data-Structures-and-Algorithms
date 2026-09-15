0ee24b72d958c2d5f40da9b6c8b9e9e5663997dc
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-15 10:52:37 +0530

    Time: 0 ms (100%), Space: 11.1 MB (71.62%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -12,23 +12,23 @@ class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
         if(left==right) return head;
+        ListNode* newHead = head;
         ListNode* first = NULL;
         ListNode* second = NULL;
         ListNode* third = NULL;
         ListNode* fourth = NULL;
         int count = 1;
-        ListNode* temp2 = head;
-        while(temp2!=NULL){
-            if(count==left-1) first = temp2;
-            if(count==left) second = temp2;
-            if(count==right) third = temp2;
-            if(count==right+1) fourth = temp2;
-            temp2 = temp2->next;
+        while(head!=NULL){
+            if(count==left-1) first = head;
+            if(count==left) second = head;
+            if(count==right) third = head;
+            if(count==right+1) fourth = head;
             count++;
+            head = head->next;
         }
         if(left==1){
             third->next = NULL;
-            ListNode* s = second;
+            ListNode* tt = second;
             ListNode* prev = NULL;
             ListNode* curr = second;
             ListNode* temp = second;
@@ -38,14 +38,11 @@ public:
                 prev = curr;
                 curr = temp;
             }
-            if(s!=NULL) s->next = fourth;
+            tt->next = fourth;
             return prev;
-
         }
-
         first->next = NULL;
         third->next = NULL;
-        ListNode* save = second;
         ListNode* prev = NULL;
         ListNode* curr = second;
         ListNode* temp = second;
@@ -56,8 +53,12 @@ public:
             curr = temp;
         }
         first->next = prev;
-        save->next = fourth;
+        ListNode* t = prev;
+        while(t->next!=NULL){
+            t = t->next;
+        }
+        t->next = fourth;
+        return newHead;
 
-        return head;
     }
 };
