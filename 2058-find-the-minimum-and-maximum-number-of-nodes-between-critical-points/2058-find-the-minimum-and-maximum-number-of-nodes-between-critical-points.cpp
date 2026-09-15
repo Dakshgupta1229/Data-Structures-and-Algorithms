12017e05b5e5b0acfb79624f58542a238675c1b4
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-15 11:29:52 +0530

    Time: 4 ms (55.96%), Space: 128.5 MB (30.09%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -12,24 +12,26 @@ class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
         vector<int> v;
-        int count = 2;
         int first = head->val;
         head = head->next;
+        int count = 2;
         while(head->next!=NULL){
             int second = head->val;
             int third = head->next->val;
+            if(second<first && second<third) v.push_back(count);
             if(second>first && second>third) v.push_back(count);
-            else if(second<first && second<third) v.push_back(count);
+            count++;
             first = second;
             head = head->next;
-            count++;
         }
-        if(v.size()<2) return {-1,-1};
+        if(v.size()==0 || v.size()==1) return {-1,-1};
+        int max_distance = v[v.size()-1] - v[0];
         int min_distance = INT_MAX;
         for(int i=1;i<v.size();i++){
-            if(min_distance>v[i]-v[i-1]) min_distance = v[i] - v[i-1];
+            if(min_distance>(v[i] - v[i-1])){
+                min_distance = v[i] - v[i-1];
+            }
         }
-        
-        return {min_distance,v[v.size()-1]-v[0]};
+        return {min_distance,max_distance};
     }
 };
