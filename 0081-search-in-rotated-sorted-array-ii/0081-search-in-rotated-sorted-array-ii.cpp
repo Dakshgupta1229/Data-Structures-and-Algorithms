198e0aa7c2bdc01ccf6f16677efc95fc7ef9d81c
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-17 19:06:09 +0530

    Time: 0 ms (100%), Space: 18 MB (80.52%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        if(nums.size()==2){
+        if(nums.size()==1){
             if(nums[0]==target) return true;
-            if(nums[1]==target) return true;
-            return false;
+            else return false;
+        }
+        if(nums.size()==2){
+            if(nums[0]==target || nums[1]==target) return true;
+            else return false;
         }
         int pivot_idx = -1;
         int low = 0;
         int high = nums.size()-1;
         while(low<=high){
             int mid = low + (high-low)/2;
-            if(nums[mid]==target || nums[low]==target || nums[high]==target) return true;
-            if(mid==0) low++;
-            else if(mid==nums.size()-1) high--;
+            if(mid==0) low = mid + 1;
+            else if(mid==nums.size()-1) high = mid - 1;
+            else if(nums[mid]==target || nums[low]==target || nums[high]==target) return true;
             else if(nums[mid]==nums[low] && nums[mid]==nums[high]){
                 low++;
                 high--;
             }
             else if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
-                pivot_idx = mid + 1;
+                pivot_idx = mid+1;
                 if(nums[mid]==nums[mid+1]) low = mid + 1;
                 else break;
             }
             else if(nums[mid]<nums[mid-1] && nums[mid]<nums[mid+1]){
                 pivot_idx = mid;
-                if(nums[mid]==nums[mid-1]) high = mid - 1;
+                if(nums[mid]==nums[mid+1]) high = mid - 1;
                 else break;
             }
             else if(nums[mid]>=nums[0]) low = mid + 1;
             else high = mid - 1;
         }
-        cout<<pivot_idx<<endl;
         if(pivot_idx==-1){
             low = 0;
             high = nums.size()-1;
