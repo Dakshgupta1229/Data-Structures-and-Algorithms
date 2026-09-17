7009027ff5165fb5a03bdeca60139243b87c151f
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-17 14:17:27 +0530

    Time: 12 ms (26.03%), Space: 22.9 MB (90.03%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
 
-    long long check(vector<int> &piles,long long h,long long mid){
+    bool check(vector<int> &piles,long long mid,long long h){
         long long count = 0;
         for(int i=0;i<piles.size();i++){
             if((long long)piles[i]<=mid) count++;
             else if((long long)piles[i]%mid==0) count = count + ((long long)piles[i]/mid);
             else count = count + ((long long)piles[i]/mid) + 1;
         }
-        return count;
+        if(count<=h) return true;
+        return false;
     }
 
     int minEatingSpeed(vector<int>& piles, int h) {
-        long long low = 1;
-        long long max_ele = LLONG_MIN;
+        int max_ele = INT_MIN;
         for(int i=0;i<piles.size();i++){
             if(max_ele<piles[i]) max_ele = piles[i];
         }
+        long long low = 1;
         long long high = max_ele;
         long long ans = -1;
         while(low<=high){
             long long mid = low + (high-low)/2;
-            long long count = check(piles,h,mid);
-            if(count<=(long long)h){
+            if(check(piles,mid,h)){
                 ans = mid;
                 high = mid - 1;
             }
             else low = mid + 1;
         }
-        return (int)ans;
+        return ans;
     }
 };
