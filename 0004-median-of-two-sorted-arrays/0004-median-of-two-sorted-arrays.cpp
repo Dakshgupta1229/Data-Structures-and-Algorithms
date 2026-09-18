00b158bc2a1c75916360df9ae351fd95ffdfa8ce
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-18 10:04:26 +0530

    Time: 2 ms (31.56%), Space: 95.7 MB (32.86%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size() + nums2.size();
-        int idx1 = n/2;
-        int idx2 = n/2-1;
+        vector<int> v;
         int i=0;
         int j=0;
-        int count = 0;
-        double val1 = 0;
-        double val2 = 0;
-        // cout<<idx1<<" "<<idx2<<endl;
         while(i<nums1.size() && j<nums2.size()){
             if(nums1[i]<nums2[j]){
-                if(count==idx1) val1 = (double)nums1[i];
-                if(count==idx2) val2 = (double)nums1[i];
+                v.push_back(nums1[i]);
                 i++;
             }
             else{
-                if(count==idx1) val1 = (double)nums2[j];
-                if(count==idx2) val2 = (double)nums2[j];
+                v.push_back(nums2[j]);
                 j++;
             }
-            count++;
-            if(count>idx1) break;
         }
-        cout<<val1<<" "<<val2;
-        while(i<nums1.size() && count<=idx1){
-            if(count==idx1) val1 = (double)nums1[i];
-            if(count==idx2) val2 = (double)nums1[i];
+        while(i<nums1.size()){
+            v.push_back(nums1[i]);
             i++;
-            count++;
         }
-        while(j<nums2.size() && count<=idx1){
-            if(count==idx1) val1 = (double)nums2[j];
-            if(count==idx2) val2 = (double)nums2[j];
+        while(j<nums2.size()){
+            v.push_back(nums2[j]);
             j++;
-            count++;
         }
-        if(n%2!=0) return (double)val1;
-        return (double)(val1 + val2)/2.0;
+        int idx = v.size()/2;
+        if(v.size()%2!=0) return (double)v[idx];
+        return (double)((v[idx-1] + v[idx])/2.0);
     }
 };
