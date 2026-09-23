7d7f704f0b6a9e2dfb59441612e559b6a8bee6fa
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-23 17:52:22 +0530

    Time: 5 ms (64.52%), Space: 15.2 MB (30.2%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,50 +17,52 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        Node* temp = head;
-        Node* copy = new Node(10);
-        Node* t = copy;
-        while(temp!=NULL){
-            Node* tt = new Node(temp->val);
-            copy->next = tt;
-            copy = copy->next;
+        Node* temp = new Node(10);
+        Node* t = temp;
+        Node* temp2 = head;
+        while(temp2!=NULL){
+            Node* tt = new Node(temp2->val);
+            temp->next = tt;
             temp = temp->next;
+            temp2 = temp2->next;
         }
         t = t->next;
-
-        Node* temp2 = new Node(10);
-        Node* temp3 = temp2;
-        while(head!=NULL && t!=NULL){
-            temp2->next = head;
+        Node* temp3 = new Node(10);
+        Node* temp4 = temp3;
+        while(t!=NULL && head!=NULL){
+            temp3->next = head;
+            temp3 = temp3->next;
             head = head->next;
-            temp2 = temp2->next;
-            temp2->next = t;
+            temp3->next = t;
+            temp3 = temp3->next;
             t = t->next;
-            temp2 = temp2->next;
         }
-        temp3 = temp3->next;
-        Node* temp8 = temp3;
-        while(temp3!=NULL){
-            Node* r = temp3->random;
-            if(r==NULL) temp3->next->random = r;
-            else temp3->next->random = r->next;
-            temp3 = temp3->next->next;
-        }
-        Node* temp4 = new Node(10);
+        temp4 = temp4->next;
         Node* temp5 = temp4;
-        Node* temp6 = new Node(10);
-        Node* temp7 = temp6;
-        while(temp8!=NULL){
-            temp4->next = temp8;
-            temp8 = temp8->next;
-            temp4 = temp4->next;
-            temp6->next = temp8;
-            temp8 = temp8->next;
-            temp6 = temp6->next;
+        while(temp5!=NULL){
+            Node* t = temp5->random;
+            if(t==NULL) temp5->next->random = NULL;
+            else{
+                temp5->next->random = t->next;
+            }
+            temp5 = temp5->next->next;
         }
-        temp4->next = NULL;
-        temp6->next = NULL;
-        head = temp5->next;
-        return temp7->next;
+        Node* temp6 = new Node(10);
+        Node* tt6 = temp6;
+        Node* temp7 = new Node(10);
+        Node* tt7 = temp7;
+        while(temp4!=NULL){
+            temp6->next = temp4;
+            temp6 = temp6->next;
+            temp4 = temp4->next;
+            temp7->next = temp4;
+            temp7 = temp7->next;
+            temp4 = temp4->next;
+        }
+        if(temp6!=NULL) temp6->next = NULL;
+        if(temp7!=NULL) temp7->next = NULL;
+        head = tt6->next;
+        return tt7->next;
+
     }
 };
