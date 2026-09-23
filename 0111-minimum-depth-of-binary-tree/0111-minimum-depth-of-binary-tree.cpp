59c2c2a8b272dc7d2bd89760f25fffddcf26cb9d
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-23 22:21:06 +0530

    Time: 3 ms (53.48%), Space: 146.5 MB (99.69%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -12,22 +12,22 @@
 class Solution {
 public:
 
-    void traverse(TreeNode* root,int count,int &min_val){
+    void traverse(TreeNode* root,int count,int &min_height){
         if(root==NULL) return;
         if(root->left==NULL && root->right==NULL){
             count++;
-            if(min_val>count) min_val = count;
+            if(min_height>count) min_height = count;
             return;
         }
-        traverse(root->left,count+1,min_val);
-        traverse(root->right,count+1,min_val);
+        traverse(root->left,count+1,min_height);
+        traverse(root->right,count+1,min_height);
     }
 
     int minDepth(TreeNode* root) {
-        int min_val = INT_MAX;
+        int min_height = INT_MAX;
         int count = 0;
-        traverse(root,count,min_val);
-        if(min_val==INT_MAX) return 0;
-        return min_val;
+        traverse(root,count,min_height);
+        if(min_height==INT_MAX) return 0;
+        return min_height;
     }
 };
