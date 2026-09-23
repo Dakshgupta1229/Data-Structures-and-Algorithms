d01c69dfa86760402428e4f140107fac1e020066
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-23 16:16:00 +0530

    Time: 99 ms (12.98%), Space: 18.5 MB (48.89%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,6 +10,27 @@
  */
 class Solution {
 public:
+
+    ListNode* merge(ListNode* list1,ListNode* list2){
+        ListNode* list = new ListNode(10);
+        ListNode* l = list;
+        while(list1!=NULL && list2!=NULL){
+            if(list1->val<list2->val){
+                list->next = list1;
+                list = list->next;
+                list1 = list1->next;
+            }
+            else{
+                list->next = list2;
+                list = list->next;
+                list2 = list2->next;
+            }
+        }
+        if(list1!=NULL) list->next = list1;
+        if(list2!=NULL) list->next = list2;
+        return l->next;
+    }
+
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if(lists.size()==0) return NULL;
         while(lists.size()>1){
@@ -17,22 +38,8 @@ public:
             ListNode* temp2 = lists[lists.size()-2];
             lists.pop_back();
             lists.pop_back();
-            ListNode* newHead = new ListNode(100);
-            ListNode* t = newHead;
-            while(temp1!=NULL && temp2!=NULL){
-                if(temp1->val<temp2->val){
-                    newHead->next = temp1;
-                    temp1 = temp1->next;
-                }
-                else{
-                    newHead->next = temp2;
-                    temp2 = temp2->next;
-                }
-                newHead = newHead->next;
-            }
-            if(temp1!=NULL) newHead->next = temp1;
-            if(temp2!=NULL) newHead->next = temp2;
-            lists.push_back(t->next);
+            ListNode* list = merge(temp1,temp2);
+            lists.push_back(list);
         }
         return lists[0];
     }
