7e2a2dbe71c9b5c9d0c4a431119fe76be3873d0e
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-24 19:23:21 +0530

    Time: 11 ms (8.15%), Space: 15.4 MB (48.44%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -20,29 +20,28 @@ public:
 
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         vector<int> v;
-        set<TreeNode*> s;
         map<TreeNode*,TreeNode*> m;
         traverse(root,m);
         queue<pair<TreeNode*,int>> q;
         q.push({target,0});
+        set<TreeNode*> s;
         s.insert(target);
         while(q.size()>0){
             auto it = q.front();
             q.pop();
             TreeNode* temp = it.first;
-            int value = temp->val;
-            int distance = it.second;
-            if(distance==k) v.push_back(value);
+            int curr = it.second;
+            if(curr==k) v.push_back(temp->val);
             if(temp->left!=NULL && s.find(temp->left)==s.end()){
-                q.push({temp->left,distance+1});
+                q.push({temp->left,curr+1});
                 s.insert(temp->left);
             }
             if(temp->right!=NULL && s.find(temp->right)==s.end()){
-                q.push({temp->right,distance+1});
+                q.push({temp->right,curr+1});
                 s.insert(temp->right);
             }
             if(m.find(temp)!=m.end() && s.find(m[temp])==s.end()){
-                q.push({m[temp],distance+1});
+                q.push({m[temp],curr+1});
                 s.insert(m[temp]);
             }
         }
