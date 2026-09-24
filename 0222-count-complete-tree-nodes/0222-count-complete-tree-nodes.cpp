003178f5ae01a3e4ca34607d9d45e6fd67321088
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-24 21:34:10 +0530

    Time: 0 ms (100%), Space: 31.3 MB (73.76%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,12 +11,12 @@
  */
 class Solution {
 public:
-
+    
     int left_tree(TreeNode* root){
         int count = 0;
         while(root!=NULL){
-            root = root->left;
             count++;
+            root = root->left;
         }
         return count;
     }
@@ -24,17 +24,17 @@ public:
     int right_tree(TreeNode* root){
         int count = 0;
         while(root!=NULL){
-            root = root->right;
             count++;
+            root = root->right;
         }
         return count;
     }
 
     int countNodes(TreeNode* root) {
         if(root==NULL) return 0;
-        int lt = left_tree(root);
-        int rt = right_tree(root);
-        if(lt==rt) return pow(2,lt) - 1;
+        int lh = left_tree(root);
+        int rh = right_tree(root);
+        if(lh==rh) return pow(2,lh) - 1;
         return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
