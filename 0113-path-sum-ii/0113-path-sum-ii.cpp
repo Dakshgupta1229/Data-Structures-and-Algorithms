2337d08b00fb79fe6296212fe2e1eaeffc32a705
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2026-09-24 08:15:16 +0530

    Time: 11 ms (25.56%), Space: 37.9 MB (22.05%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -11,24 +11,27 @@
  */
 class Solution {
 public:
-
-    void traverse(TreeNode* root,int target,int sum,vector<int> v1,vector<vector<int>> &v){
+    
+    void traverse(TreeNode* root,int targetSum,int sum,vector<int> v1,vector<vector<int>> &v){
         if(root==NULL) return;
         if(root->left==NULL && root->right==NULL){
-            v1.push_back(root->val);
             sum = sum + root->val;
-            if(sum==target) v.push_back(v1);
+            if(sum==targetSum){
+                v1.push_back(root->val);
+                v.push_back(v1);
+            }
             return;
         }
         v1.push_back(root->val);
-        traverse(root->left,target,sum+root->val,v1,v);
-        traverse(root->right,target,sum+root->val,v1,v);
+        traverse(root->left,targetSum,sum+root->val,v1,v);
+        traverse(root->right,targetSum,sum+root->val,v1,v);
     }
 
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<vector<int>> v;
         vector<int> v1;
-        traverse(root,targetSum,0,v1,v);
+        int sum = 0;
+        traverse(root,targetSum,sum,v1,v);
         return v;
     }
 };
